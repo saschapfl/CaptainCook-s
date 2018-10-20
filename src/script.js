6172560f782c1f616821f0049e21286d5f1ee71a
Sascha <s.pflittner@gmail.com>
2018-10-20 13:48:51 +0200

    script_js_03

--- src/script.js
+++ src/script.js
@@ -49,14 +49,29 @@ let backtoHome = () =>{
 }
 
 
+let switchtoRecipe = (event) =>{
+    let aktuelleSeite = document.querySelector(".active");
+    aktuelleSeite.classList.remove("active");
+    aktuelleSeite.classList.add("hidden");
+    let Rezeptansicht = document.querySelector("#Rezept_anzeigen");
+    Rezeptansicht.classList.remove("hidden");
+    Rezeptansicht.classList.add("active");
+    let name = event.target.getAttribute("id");
+    alert(name);
+
+}
+
 
 window.addEventListener("load", () => {
    let modal = document.querySelector(".modal");
    let trigger = document.querySelector(".trigger");
    let closeButton = document.querySelector(".close-button");
-
+   let rezeptliste = document.querySelectorAll("#Rezeptliste > div");
+   for(let i = 0; i < rezeptliste.length;i++){
+     rezeptliste[i].addEventListener("click", switchtoRecipe);
+   }
    let toggleModal = () =>{
-       modal.classList.toggle("show-modal");
+          modal.classList.toggle("show-modal");
    }
    let windowOnClick = (event) =>{
        if (event.target === modal) {
