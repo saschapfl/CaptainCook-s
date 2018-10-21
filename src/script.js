8560c92bd6a9f583651197172da96f2fde8cc70d
Sascha <s.pflittner@gmail.com>
2018-10-21 12:54:53 +0200

    script_js_05

--- src/script.js
+++ src/script.js
@@ -1,70 +1,3 @@
-let switchtoaddRecipe = () =>{
-    let Startseite = document.querySelector("#Startseite");
-    Startseite.classList.remove("active");
-    Startseite.classList.add("hidden");
-    let Rezept_hinzufuegen = document.querySelector("#Rezept_hinzufuegen");
-    Rezept_hinzufuegen.classList.remove("hidden");
-    Rezept_hinzufuegen.classList.add("active");
-
-}
-let switchtolist = (source) =>{
-      let Startseite = document.querySelector("#Startseite");
-      Startseite.classList.remove("active");
-      Startseite.classList.add("hidden");
-      let Rezeptliste = document.querySelector("#Rezeptliste");
-      Rezeptliste.classList.remove("hidden");
-      Rezeptliste.classList.add("active");
-      let dropdown = document.querySelector("#dropdown");
-      dropdown.classList.remove("hidden");
-      dropdown.classList.add("active");
-      let image = source.getAttribute("src");
-      switch(image){
-        case "schnelle_Rezepte.jpg":
-        alert("schnell");
-        break;
-        case "vegetarische_Rezepte.jpg":
-        alert("veggi");
-        break;
-        case "mediterrane_Rezepte.jpg":
-        alert("medi");
-        break;
-        case "Pizza_Pasta.jpg":
-        alert("Pizza");
-        break;
-        case "alle_Rezepte.jpg":
-        alert("alle");
-        break;
-        case "scharfe_Rezepte.jpg":
-        alert("hot");
-        break;
-
-      }
-    }
-
-let backtoHome = () =>{
-    let activClass = document.querySelector(".active");
-    activClass.classList.remove("active");
-    activClass.classList.add("hidden");
-
-    let Startseite = document.querySelector("#Startseite");
-    Startseite.classList.remove("hidden");
-    Startseite.classList.add("active");
-}
-
-
-let switchtoRecipe = (event) =>{
-    let aktuelleSeite = document.querySelector(".active");
-    aktuelleSeite.classList.remove("active");
-    aktuelleSeite.classList.add("hidden");
-    let Rezeptansicht = document.querySelector("#Rezept_anzeigen");
-    Rezeptansicht.classList.remove("hidden");
-    Rezeptansicht.classList.add("active");
-    let name = event.target.getAttribute("id");
-    alert(name);
-
-}
-
-
 window.addEventListener("load", () => {
    let modal = document.querySelector(".modal");
    let trigger = document.querySelector(".trigger");
@@ -86,3 +19,92 @@ window.addEventListener("load", () => {
    closeButton.addEventListener("click", toggleModal);
    window.addEventListener("click", windowOnClick);
 });
+
+let deleteRecipe = (source) =>{
+  
+}
+
+let switchtoaddRecipe = () =>{
+    let Startseite = document.querySelector("#Startseite");
+    Startseite.classList.remove("active");
+    Startseite.classList.add("hidden");
+    let Rezept_hinzufuegen = document.querySelector("#Rezept_hinzufuegen");
+    Rezept_hinzufuegen.classList.remove("hidden");
+    Rezept_hinzufuegen.classList.add("active");
+
+
+}
+let switchtolist = (source) =>{
+      let Startseite = document.querySelector("#Startseite");
+      Startseite.classList.remove("active");
+      Startseite.classList.add("hidden");
+      let Rezeptliste = document.querySelector("#Rezeptliste");
+      Rezeptliste.classList.remove("hidden");
+      Rezeptliste.classList.add("active");
+      let dropdown = document.querySelector("#dropdown");
+      dropdown.classList.remove("hidden");
+
+      //Eingabefeld holen und je nach Bild Wert anpassen
+      let input = document.querySelector("#Suche");
+      let image = source.getAttribute("src");
+      switch(image){
+        case "schnelle_Rezepte.jpg":
+        input.setAttribute("value", "Schnelle Rezepte");
+        break;
+        case "vegetarische_Rezepte.jpg":
+        input.setAttribute("value", "Vegetarische Rezepte");
+        break;
+        case "mediterrane_Rezepte.jpg":
+        input.setAttribute("value", "Mediterrane Rezepte");
+        break;
+        case "Pizza_Pasta.jpg":
+        input.setAttribute("value", "Pizza und Pasta");
+        break;
+        case "alle_Rezepte.jpg":
+        input.setAttribute("value", "Alle Rezepte");
+        break;
+        case "scharfe_Rezepte.jpg":
+        input.setAttribute("value", "Scharfe Rezepte");
+        break;
+
+      }
+    }
+
+let backtoHome = () =>{
+    let activClass = document.querySelector(".active");
+    activClass.classList.remove("active");
+    activClass.classList.add("hidden");
+
+    let dropdown = document.querySelector("#dropdown");
+    dropdown.classList.add("hidden");
+
+    let Startseite = document.querySelector("#Startseite");
+    Startseite.classList.remove("hidden");
+    Startseite.classList.add("active");
+
+    //Wert der Suchleiste zurücksetzen
+    let input = document.querySelector("#Suche");
+    input.setAttribute("value", "");
+}
+
+
+let switchtoRecipe = (event) =>{
+  //Seite switchen
+   let aktuelleSeite = document.querySelector(".active");
+   aktuelleSeite.classList.remove("active");
+   aktuelleSeite.classList.add("hidden");
+   let Rezeptansicht = document.querySelector("#Rezept_anzeigen");
+   Rezeptansicht.classList.remove("hidden");
+   Rezeptansicht.classList.add("active");
+
+   //Dropdown-Button verstecken
+   let dropdown = document.querySelector("#dropdown");
+   dropdown.classList.add("hidden");
+
+   //Das gewünschte Rezept initialisieren
+   let id = event.currentTarget.getAttribute("id");
+
+   //In Suchleiste aktuellen Rezeptnamen eintragen
+   let input = document.querySelector("#Suche");
+   input.setAttribute("value", );
+}
